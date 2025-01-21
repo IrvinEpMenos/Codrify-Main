509bfe34c52ec66fbc47fa2627d7ac781b181ee5
JonathanMN <jonathan_4.2003@hotmail.com>
2025-01-20 21:25:25 -0600

    Navbar CSS
    Cambios en el header en diseño e implementacion el boton de idioma

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,32 +1,35 @@
 // Navbar.tsx
 import React from 'react';
-import { Link } from 'react-router-dom'; // Usar Link de React Router
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 import logo from '../assets/img/logoblanco.svg';
 
 const Navbar: React.FC = () => {
   return (
+    <header className='header-link'>
     <nav className="navbar">
       <div className="navbar-logo">
         <Link to="/">
           <img src={logo} alt="Logo" className="logo-image" />
         </Link>
       </div>
+      <div className='oval-container'>
       <ul className="navbar-links">
-        <li>
-          <Link to="/">Inicio</Link>
-        </li>
-        <li>
-          <Link to="/acerca">Nosotros</Link>
-        </li>
-        <li>
-          <Link to="/servicios">Servicios</Link>
-        </li>
-        <li>
-          <Link to="/contacto">Contacto</Link>
-        </li>
+        <li><Link to="/">Inicio</Link></li>
+        <li><Link to="/acerca">Nosotros</Link></li>
+        <li><Link to="/servicios">Servicios</Link></li>
+        <li><Link to="/contacto">Contacto</Link></li>
+        <div className='language-selector'>
+      <select>
+        <option value='es'>Español</option>
+        <option value='en'>English</option>
+      </select>
+    </div>
       </ul>
+      </div>
     </nav>
+    </header>
+
   );
 };
 
