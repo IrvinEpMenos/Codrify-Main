d4090b5c08e4ef0fe4168620c51056713ee39430
JonathanMN <jonathan_4.2003@hotmail.com>
2025-01-21 20:21:04 -0600

    Navbar completo
    Cambios y terminacion del header

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,35 +1,53 @@
 // Navbar.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import logo from '../assets/img/logoblanco.svg';
+import mexicoFlag from '../assets/icon/mexico.svg';
+import usaFlag from '../assets/icon/usa.svg';
 
 const Navbar: React.FC = () => {
+  const [language, setLanguage] = useState('es');
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const handleLanguageChange = (lang: string) => {
+    setLanguage(lang);
+    setDropdownOpen(false);
+  };
+
   return (
     <header className='header-link'>
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <Link to="/">
-          <img src={logo} alt="Logo" className="logo-image" />
-        </Link>
-      </div>
-      <div className='oval-container'>
-      <ul className="navbar-links">
-        <li><Link to="/">Inicio</Link></li>
-        <li><Link to="/acerca">Nosotros</Link></li>
-        <li><Link to="/servicios">Servicios</Link></li>
-        <li><Link to="/contacto">Contacto</Link></li>
-        <div className='language-selector'>
-      <select>
-        <option value='es'>Español</option>
-        <option value='en'>English</option>
-      </select>
-    </div>
-      </ul>
-      </div>
-    </nav>
+      <nav className="navbar">
+        <div className="navbar-logo">
+          <Link to="/">
+            <img src={logo} alt="Logo" className="logo-image" />
+          </Link>
+        </div>
+        <div className='oval-container'>
+          <ul className="navbar-links">
+            <li><Link to="/">Inicio</Link></li>
+            <li><Link to="/acerca">Nosotros</Link></li>
+            <li><Link to="/servicios">Servicios</Link></li>
+            <div className='oval-container1'><li><Link to="/contacto">Contacto</Link></li></div>
+          </ul>
+        </div>
+        <div className='language-dropdown'>
+              <div className='language-select' onClick={() => setDropdownOpen(!dropdownOpen)}>
+                <img src={language === 'es' ? mexicoFlag : usaFlag} alt={language === 'es' ? "Español" : "Inglés"} className="flag-icon" />
+              </div>
+              {dropdownOpen && (
+                <ul className='language-options'>
+                  <li className='language-option' onClick={() => handleLanguageChange('es')}>
+                    <img src={mexicoFlag} alt="Español" className="flag-iconmx" />
+                  </li>
+                  <li className='language-option' onClick={() => handleLanguageChange('en')}>
+                    <img src={usaFlag} alt="Inglés" className="flag-icon" />
+                  </li>
+                </ul>
+              )}
+            </div>
+      </nav>
     </header>
-
   );
 };
 
