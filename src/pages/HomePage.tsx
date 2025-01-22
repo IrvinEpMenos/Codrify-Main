389441c16e69b3a26bff9d68ce669bd0cbadbc0d
Irvin Sanchez Ayala <70862981+IrvinEpMenos@users.noreply.github.com>
2025-01-22 11:38:25 -0600

    imagen
    añadi nuevo logo

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,12 @@
 // pages/HomePage.tsx
 import React from 'react';
+import './css/Home.css';
 
 const HomePage: React.FC = () => {
   return (
-    <div>
-      <h1>Bienvenido a la Página de Inicio</h1>
-      <p>Este es el contenido principal de la página de inicio.</p>
-    </div>
+<div className="container">
+  
+</div>
   );
 };
 
