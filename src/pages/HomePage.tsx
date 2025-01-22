cb47c9b55dcc6444c97dbb8007ffba3cc7728dc4
Irvin Sanchez Ayala <70862981+IrvinEpMenos@users.noreply.github.com>
2025-01-22 12:08:49 -0600

    warning libreria
    noooooooo libririas noooo

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,30 @@
 // pages/HomePage.tsx
 import React from 'react';
 import './css/Home.css';
+import { motion } from "framer-motion";
+
+const Animacion = () => (
+  <motion.div
+    animate={{ x: 100 }}
+    transition={{ duration: 1 }}
+  >
+    ¡Hola Mundo!
+  </motion.div>
+);
+
+
 
 const HomePage: React.FC = () => {
   return (
 <div className="container">
-  
+  <div className="home">
+    <div className='home-title'>
+      <h1 className="">Bienvenido a la página de inicio</h1>
+      <div className='video-container'>
+        <Animacion />
+      </div>
+    </div>
+  </div>
 </div>
   );
 };
