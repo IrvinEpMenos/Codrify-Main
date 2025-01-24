57251560b0b1adee26e528bbbb984f2b4eaa3a00
xThunderking <reynarayonacho850@gmail.com>
2025-01-24 13:29:57 -0600

    se

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,63 +1,35 @@
-"use client";
-
-import { 
-    motion,
-    MotionValue,
-    
- } from "framer-motion";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Ejemplo de animación con framer-motion
 // import { m, motion } from "framer-motion";
 
-// const Test = () => {
-//   return (
-//     <div>
-//       <motion.div
-//         initial={{ opacity: 0, scale: 0 }}
-//         animate={{ opacity: 1, scale: 1 }}
-//       transition={{
-//         repeat: Infinity,
-//         duration: 4,
-//         repeatType: "reverse",
+import { motion } from "framer-motion";
+
+const Test = () => {
+  return (
+    <div>
+      <motion.div
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        repeat: Infinity,
+        duration: 4,
+        repeatType: "reverse",
         
-//       }}
-//       style={ball}
-//       >
-//       </motion.div>
-//     </div>
-//   );
-// }
+      }}
+      style={ball}
+      >
+      </motion.div>
+    </div>
+  );
+}
 
-// export default Test;
+export default Test;
 
-// //styles
-// const ball = {
-//   margin: "auto",
-//   width: 100,
-//   height: 100,
-//   border: "2px solid blue",
-//   borderRadius: "50%",
-// };
+//styles
+const ball = {
+  margin: "auto",
+  width: 100,
+  height: 100,
+  border: "2px solid blue",
+  borderRadius: "50%",
+};
