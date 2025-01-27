4d7d2f7d78c87f8a333d81abc9e968f41c940cdf
JonathanMN <jonathan_4.2003@hotmail.com>
2025-01-27 14:18:25 -0600

    Nabvar y Footter responsive
    Fase final de hacer responsivo el Header y el fotter

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,3 @@
-// Navbar.tsx
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
@@ -9,6 +8,7 @@ import usaFlag from '../assets/icon/usa.svg';
 const Navbar: React.FC = () => {
   const [language, setLanguage] = useState('es');
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleLanguageChange = (lang: string) => {
     setLanguage(lang);
@@ -17,17 +17,22 @@ const Navbar: React.FC = () => {
 
   return (
     <header className='header-link'>
-      <nav className="navbar">
+      <nav className={`navbar ${menuOpen ? 'active' : ''}`}>
         <div className="navbar-logo">
           <Link to="/">
             <img src={logo} alt="Logo" className="logo-image" />
           </Link>
         </div>
-        <div className='oval-container'>
+        <div className='hamburger-menu' onClick={() => setMenuOpen(!menuOpen)}>
+          <span className={`bar ${menuOpen ? 'active' : ''}`}></span>
+          <span className={`bar ${menuOpen ? 'active' : ''}`}></span>
+          <span className={`bar ${menuOpen ? 'active' : ''}`}></span>
+        </div>
+        <div className={`oval-container ${menuOpen ? 'active' : ''}`}>
           <ul className="navbar-links">
             <li className='li-text'><Link to="/">Inicio</Link></li>
-            <li  className='li-text'><Link to="/acerca">Nosotros</Link></li>
-            <li  className='li-text'><Link to="/servicios">Servicios</Link></li>
+            <li className='li-text'><Link to="/acerca">Nosotros</Link></li>
+            <li className='li-text'><Link to="/servicios">Servicios</Link></li>
             <div className='oval-container1'><li><Link to="/contacto">Contacto</Link></li></div>
           </ul>
         </div>
