52531469b44879866dbb045ce65c22ce89cb85c1
JonathanMN <jonathan_4.2003@hotmail.com>
2025-02-05 15:05:39 -0600

    Servicis LandinfPage Part 2
    Avances Services en la pagina de Inicia

--- src/pages/landingPage/OurServices.tsx
+++ src/pages/landingPage/OurServices.tsx
@@ -8,36 +8,42 @@ export default function Services() {
     <section className="services-Landing">
       <h2>Nuestros Servicios</h2>
       <p className="subtitle">Todo lo que necesitas - en un solo sitio.</p>
-      <div className="container-services">
+      <div className="container-services1">
         <div className="service-item1">
-          <span className="icon"><img src={UIUX} alt="" /></span>
+          <span className="icon-serviceLanding"><img src={UIUX} alt="" /></span>
           <div>
             <h3 className="service-title">Diseño UI/UX</h3>
             <p className="service-description">Creamos interfaces modernas, únicas y funcionales que mejoran la experiencia de usuario y refuerzan la identidad de tu marca.</p>
           </div>
         </div>
+        </div>
+        <div className="container-services2">
         <div className="service-item2">
-          <span className="icon"><img src={stats} alt="" /></span>
+          <span className="icon-serviceLanding"><img src={stats} alt="" /></span>
           <div>
             <h3 className="service-title">Plan Estratégico Personalizado</h3>
             <p className="service-description">Diseñamos estrategias tecnológicas a medida, alineadas con los objetivos de tu empresa para maximizar su potencial.</p>
           </div>
         </div>
+        </div>
+        <div className="container-services3">
         <div className="service-item3">
-          <span className="icon"><img src={nut} alt="" /></span>
+          <span className="icon-serviceLanding"><img src={nut} alt="" /></span>
           <div>
             <h3 className="service-title">Implementaciones Tecnológicas</h3>
             <p className="service-description">Desarrollamos soluciones digitales personalizadas para optimizar tus procesos y modernizar tu negocio.</p>
           </div>
         </div>
+        </div>
+        <div className="container-services4">
         <div className="service-item4">
-          <span className="icon"><img src={cerebro} alt="" /></span>
+          <span className="icon-serviceLanding"><img src={cerebro} alt="" /></span>
           <div>
             <h3 className="service-title">Automatización con Inteligencia Artificial</h3>
             <p className="service-description">Integramos IA para optimizar procesos, reducir costos y tomar decisiones más inteligentes en tiempo real.</p>
           </div>
         </div>
-      </div>
+        </div>
     </section>
   );
 }
