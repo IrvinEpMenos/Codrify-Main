08f7f8c5d1ad65a158683d12a0e29be175b66d78
Irvin Sanchez Ayala <70862981+IrvinEpMenos@users.noreply.github.com>
2025-02-04 18:17:20 -0600

    Revert "ourservices part 1"
    This reverts commit 13cfa291a3c88d8eb0338ae8c494288d52ced5aa.

--- src/pages/landingPage/AboutUs.tsx
+++ src/pages/landingPage/AboutUs.tsx
@@ -1,17 +1,18 @@
-import React from "react";
 import "./css/AboutUSLanding.css";
 import Group from "../../assets/icon/Group.svg";
 import PuntosBlancos from "../../assets/img/PuntosBlancos.png";
-
-const AboutUs: React.FC = () => {
-  return (
-    <section className="about-section">
-      <img src={PuntosBlancos} alt="Puntos blancos" className="Puntos-Blancos" />
-      <img src={PuntosBlancos} alt="Puntos Blancos2" className="Puntos-Blancos2" />
-      <img src={PuntosBlancos} alt="Puntos Blancos3" className="Puntos-Blancos3" />
-
+export default function AboutUs() {
+    return (
+      <section className="about-section">
+        <img src={PuntosBlancos} alt="Puntos blancos" className="Puntos-Blancos" />
+        <img src={PuntosBlancos} alt="Puntos Blancos2"  className="Puntos-Blancos2"/>
+        <img src={PuntosBlancos} alt="Puntos Blancos2"  className="Puntos-Blancos3"/>
       <img src={Group} alt="Icono de usuario" className="about-icon" />
 
+      <svg className="curved-line" viewBox="0 0 100 200" fill="none" xmlns="http://www.w3.org/2000/svg">
+       <path d="M 120 30 Q 10 10, 10 70 L 20 400 Q 30 180, 50 200"
+        stroke="rgba(255, 255, 255, 0.5)" strokeWidth="3" fill="none"/>
+      </svg>
       <div className="about-header">
         <h2>Sobre Nosotros</h2>
       </div>
@@ -21,9 +22,8 @@ const AboutUs: React.FC = () => {
           <strong>¿Quiénes Somos?</strong>
         </h3>
         <p>
-          En Codrify Consulting, somos una firma de consultoría tecnológica dedicada a transformar el
-          potencial de las pequeñas y medianas empresas (pymes) mediante la implementación de
-          herramientas innovadoras como la inteligencia artificial, la automatización y tecnologías digitales avanzadas.
+        En Codrify Consulting, somos una firma de consultoría tecnológica dedicada a transformar el potencial de las pequeñas y medianas empresas (pymes) 
+        mediante la implementación de herramientas innovadoras como la inteligencia artificial, la automatización y tecnologías digitales avanzadas.
         </p>
 
         <div className="about-banner">
@@ -36,5 +36,3 @@ const AboutUs: React.FC = () => {
     </section>
   );
 };
-
-export default AboutUs;
