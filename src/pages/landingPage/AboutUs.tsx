0b6762de3d18cf4be94cf64b5c1b2884d4525257
JonathanMN <jonathan_4.2003@hotmail.com>
2025-02-05 11:30:51 -0600

    Restaurando
    Se restauraron los cambios de ayer con los problemas de combinaciones de clases en CSS. Todo esta arreglado
    PD: No pongan la misma

--- src/pages/landingPage/AboutUs.tsx
+++ src/pages/landingPage/AboutUs.tsx
@@ -1,15 +1,40 @@
-export default function OurClients() {
-    return (
-        <div>
-        <div className="container-middel">
-            <div className="clients">
-            <div className="clients-title">
-                <div className="clients-title-text">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic assumenda, atque sed ex tempora quidem vero quas voluptates nemo corporis. Illo labore commodi rem maxime quidem deserunt modi quas animi?
-                </div>
-            </div>
-            </div>
+import React from "react";
+import "./css/AboutUSLanding.css";
+import Group from "../../assets/icon/Group.svg";
+import PuntosBlancos from "../../assets/img/PuntosBlancos.png";
+
+const AboutUs: React.FC = () => {
+  return (
+    <section className="sect-about">
+      <img src={PuntosBlancos} alt="Puntos blancos" className="Puntos-Blancos" />
+      <img src={PuntosBlancos} alt="Puntos Blancos2" className="Puntos-Blancos2" />
+      <img src={PuntosBlancos} alt="Puntos Blancos3" className="Puntos-Blancos3" />
+
+      <img src={Group} alt="Icono de usuario" className="about-icon" />
+
+      <div className="about-header">
+        <h2>Sobre Nosotros</h2>
+      </div>
+
+      <div className="about-content">
+        <h3>
+          <strong>¿Quiénes Somos?</strong>
+        </h3>
+        <p>
+          En Codrify Consulting, somos una firma de consultoría tecnológica dedicada a transformar el
+          potencial de las pequeñas y medianas empresas (pymes) mediante la implementación de
+          herramientas innovadoras como la inteligencia artificial, la automatización y tecnologías digitales avanzadas.
+        </p>
+
+        <div className="about-banner">
+          <h1>
+            " <span className="highlight">innovamos</span> tu negocio <br />
+            <span className="highlight">transformamos</span> tu futuro "
+          </h1>
         </div>
-        </div>
-    );
-    }
+      </div>
+    </section>
+  );
+};
+
+export default AboutUs;
--- src/pages/landingPage/OurServices.tsx
+++ src/pages/landingPage/OurServices.tsx
@@ -1,15 +1,43 @@
-export default function OurServices() {
-    return (
-        <div>
-        <div className="container-middel">
-            <div className="clients">
-            <div className="clients-title">
-                <div className="clients-title-text">
-               Servicios Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic assumenda, atque sed ex tempora quidem vero quas voluptates nemo corporis. Illo labore commodi rem maxime quidem deserunt modi quas animi?
-                </div>
-            </div>
-            </div>
+import "./css/OurServices.css";
+import cerebro from "../../assets/icon/cerebro.png";
+import UIUX from "../../assets/icon/UIUXICON.svg";
+import nut from "../../assets/icon/nut.svg";
+import stats from "../../assets/icon/stats.svg";
+export default function Services() {
+  return (
+    <section className="services">
+      <h2 className="title">Nuestros Servicios</h2>
+      <p className="subtitle">Todo lo que necesitas - en un solo sitio.</p>
+      <div className="container-services">
+        <div className="service-item1">
+          <span className="icon"><img src={UIUX} alt="" /></span>
+          <div>
+            <h3 className="service-title">Diseño UI/UX</h3>
+            <p className="service-description">Creamos interfaces modernas, únicas y funcionales que mejoran la experiencia de usuario y refuerzan la identidad de tu marca.</p>
+          </div>
         </div>
+        <div className="service-item2">
+          <span className="icon"><img src={stats} alt="" /></span>
+          <div>
+            <h3 className="service-title">Plan Estratégico Personalizado</h3>
+            <p className="service-description">Diseñamos estrategias tecnológicas a medida, alineadas con los objetivos de tu empresa para maximizar su potencial.</p>
+          </div>
         </div>
-    );
-    }
+        <div className="service-item3">
+          <span className="icon"><img src={nut} alt="" /></span>
+          <div>
+            <h3 className="service-title">Implementaciones Tecnológicas</h3>
+            <p className="service-description">Desarrollamos soluciones digitales personalizadas para optimizar tus procesos y modernizar tu negocio.</p>
+          </div>
+        </div>
+        <div className="service-item4">
+          <span className="icon"><img src={cerebro} alt="" /></span>
+          <div>
+            <h3 className="service-title">Automatización con Inteligencia Artificial</h3>
+            <p className="service-description">Integramos IA para optimizar procesos, reducir costos y tomar decisiones más inteligentes en tiempo real.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
