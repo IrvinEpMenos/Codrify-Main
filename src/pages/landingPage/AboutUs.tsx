4e595f035f55db2a8ad75b127db71875a93dc0f7
JonathanMN <jonathan_4.2003@hotmail.com>
2025-02-04 18:53:31 -0600

    Quitanto Bugs
    Resolviendo problemas de diseños

--- src/pages/landingPage/AboutUs.tsx
+++ src/pages/landingPage/AboutUs.tsx
@@ -1,38 +1,15 @@
-import "./css/AboutUSLanding.css";
-import Group from "../../assets/icon/Group.svg";
-import PuntosBlancos from "../../assets/img/PuntosBlancos.png";
-export default function AboutUs() {
+export default function OurClients() {
     return (
-      <section className="about-section">
-        <img src={PuntosBlancos} alt="Puntos blancos" className="Puntos-Blancos" />
-        <img src={PuntosBlancos} alt="Puntos Blancos2"  className="Puntos-Blancos2"/>
-        <img src={PuntosBlancos} alt="Puntos Blancos2"  className="Puntos-Blancos3"/>
-      <img src={Group} alt="Icono de usuario" className="about-icon" />
-
-      <svg className="curved-line" viewBox="0 0 100 200" fill="none" xmlns="http://www.w3.org/2000/svg">
-       <path d="M 120 30 Q 10 10, 10 70 L 20 400 Q 30 180, 50 200"
-        stroke="rgba(255, 255, 255, 0.5)" strokeWidth="3" fill="none"/>
-      </svg>
-      <div className="about-header">
-        <h2>Sobre Nosotros</h2>
-      </div>
-
-      <div className="about-content">
-        <h3>
-          <strong>¿Quiénes Somos?</strong>
-        </h3>
-        <p>
-        En Codrify Consulting, somos una firma de consultoría tecnológica dedicada a transformar el potencial de las pequeñas y medianas empresas (pymes) 
-        mediante la implementación de herramientas innovadoras como la inteligencia artificial, la automatización y tecnologías digitales avanzadas.
-        </p>
-
-        <div className="about-banner">
-          <h1>
-            " <span className="highlight">innovamos</span> tu negocio <br />
-            <span className="highlight">transformamos</span> tu futuro "
-          </h1>
+        <div>
+        <div className="container-middel">
+            <div className="clients">
+            <div className="clients-title">
+                <div className="clients-title-text">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic assumenda, atque sed ex tempora quidem vero quas voluptates nemo corporis. Illo labore commodi rem maxime quidem deserunt modi quas animi?
+                </div>
+            </div>
+            </div>
         </div>
-      </div>
-    </section>
-  );
-};
+        </div>
+    );
+    }
--- src/pages/landingPage/Business.tsx
+++ src/pages/landingPage/Business.tsx
@@ -1,36 +1,15 @@
-import "./css/Business.css";
-import motor from "../../assets/img/motornegocio.jpg"
-
-export default function Business() {
+export default function OurClients() {
     return (
-        <div className="business-container">
-            {/* Logo y Título */}
-            <div className="header">
-                <div className="logo">
-                    <div className="logo-container">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 38 38" fill="none">
-                            <path d="M25.5312 5.34375C25.5312 6.72917 23.4708 7.125 21.9688 7.125C15.7736 7.125 9.256 11.2564 8.40578 16.6295C8.30327 17.2773 7.78084 17.8125 7.125 17.8125H3.5625M4.75 24.3438C5.68274 24.6547 7.27484 25.8692 8.83577 26.1458C9.51149 26.2655 10.2038 26.6964 10.3772 27.3603C10.7368 28.7373 11.0287 29.7251 10.0938 32.0625M20.4544 30.0353C19.2669 30.0353 14.5169 27.4228 14.5169 19.3478C14.5169 12.8165 18.6731 11.0353 24.6106 11.0353C28.1731 11.0353 30.5481 12.8165 30.5481 16.9728C30.5481 21.129 26.3919 20.1875 24.6106 21.9688C22.8294 23.75 24.6106 30.0353 20.4544 30.0353ZM34.4375 19C34.4375 27.5259 27.5259 34.4375 19 34.4375C10.4741 34.4375 3.5625 27.5259 3.5625 19C3.5625 10.4741 10.4741 3.5625 19 3.5625C27.5259 3.5625 34.4375 10.4741 34.4375 19Z" stroke="white" stroke-width="1.5" />
-                        </svg>
-                    </div>
+        <div>
+        <div className="container-middel">
+            <div className="clients">
+            <div className="clients-title">
+                <div className="clients-title-text">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic assumenda, atque sed ex tempora quidem vero quas voluptates nemo corporis. Illo labore commodi rem maxime quidem deserunt modi quas animi?
                 </div>
-                <br /><br /><br />
-                <h1>Codrify:</h1>
-                <h2 className="h2a">El motor de tu negocio</h2>
-                <p className="ppp">Tu socio en la transformación digital.</p>
             </div>
-
-            {/* Espaciado adicional */}
-            <div className="spacer"></div>
-
-            {/* Imagen Principal */}
-            <div className="main-image">
-                <img src={motor} alt="" />
-                <h3 className="centered-text">CODRIFY</h3>
             </div>
-
-            {/* Espaciado adicional */}
-            <div className="spacer"></div>
-
+        </div>
         </div>
     );
-}
+    }
