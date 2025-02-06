da5691ddd20a598fd3f951ffe1c1f94b9bf434c7
Irvin Sanchez Ayala <70862981+IrvinEpMenos@users.noreply.github.com>
2025-02-06 12:12:48 -0600

    screen height
    la pantalla mas grande

--- src/pages/landingPage/OurClients.tsx
+++ src/pages/landingPage/OurClients.tsx
@@ -1,24 +1,78 @@
-import { useState } from "react";
-import { motion } from "framer-motion";
+// import { useState } from "react";
+// import { motion } from "framer-motion";
 
-interface Client {
-    name: string;
-    info: string;
-}
+import React from "react";
 
-const clientsData: Client[] = [
-    { name: "Empresa 1", info: "Cliente desde 2020, especializado en software empresarial." },
-    { name: "Empresa 2", info: "Proveedor de soluciones logísticas, colaboración desde 2018." },
-    { name: "Empresa 3", info: "Socio estratégico en marketing digital desde 2019." },
-    { name: "Empresa 4", info: "Líder en manufactura, trabajando juntos desde 2021." },
-    { name: "Empresa 5", info: "Innovador en tecnología educativa, cliente desde 2017." }
-];
+// interface Client {
+//     name: string;
+//     info: string;
+// }
 
-export default function OurClients(): JSX.Element {
-    const [hoveredClient, setHoveredClient] = useState<string | null>(null);
+// const clientsData: Client[] = [
+//     { name: "Empresa 1", info: "Cliente desde 2020, especializado en software empresarial." },
+//     { name: "Empresa 2", info: "Proveedor de soluciones logísticas, colaboración desde 2018." },
+//     { name: "Empresa 3", info: "Socio estratégico en marketing digital desde 2019." },
+//     { name: "Empresa 4", info: "Líder en manufactura, trabajando juntos desde 2021." },
+//     { name: "Empresa 5", info: "Innovador en tecnología educativa, cliente desde 2017." }
+// ];
+
+const OurClients: React.FC = () => {
+    // const [hoveredClient, setHoveredClient] = useState<string | null>(null);
 
     return (
-        <div className="our-clients">
+        <div className="slider">
+            <div className="slide-track">
+                <div className="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
+                </div>
+                <div className="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+                </div>
+                <div className="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+                </div>
+                <div className="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+                </div>
+                <div className="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+                </div>
+                <div className="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+                </div>
+                <div className="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+                </div>
+                <div className="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
+                </div>
+                <div className="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+                </div>
+                <div className="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+                </div>
+                <div className="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+                </div>
+                <div className="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+                </div>
+                <div className="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+                </div>
+                <div className="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default OurClients;
+
+
+{/* <div className="our-clients">
             <div className="container-top">
                 <h1>Nuestros clientes</h1>
             </div>
@@ -45,6 +99,4 @@ export default function OurClients(): JSX.Element {
                     ))}
                 </ul>
             </div>
-        </div>
-    );
-}
+        </div> */}
