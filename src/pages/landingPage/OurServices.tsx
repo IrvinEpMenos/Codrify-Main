9f7751f975d3c43b579991c3fcaa48f8ab5372c2
JonathanMN <jonathan_4.2003@hotmail.com>
2025-02-11 14:15:22 -0600

    Introduccion de Infinity Menu
    changes the Infinity menu in ourservices

--- src/pages/landingPage/OurServices.tsx
+++ src/pages/landingPage/OurServices.tsx
@@ -1,29 +1,46 @@
-import "./css/OurServices.css";
+import InfiniteMenu from '../../../reactBits/InfiniteMenu/InfiniteMenu';
 import cerebro from "../../assets/icon/cerebro.png";
 import UIUX from "../../assets/icon/UIUXICON.svg";
 import nut from "../../assets/icon/nut.svg";
-import stats from "../../assets/icon/stats.svg";
+import stats from "../../assets/icon/stats.svg"
+
 export default function Services() {
   return (
-    <section className="serv">
-      <h2 className="title">Nuestros Servicios</h2>
-      <p className="subtitle">Todo lo que necesitas - en un solo sitio.</p>
-        <div className="serv-items">
-            <h3 className="serv-title1"> <img src={UIUX} alt="UI/UX" className="icon-servuiux" />Diseño UI/UX</h3>
-            <p className="serv-description1">Creamos interfaces modernas, únicas y funcionales que mejoran la experiencia de usuario y refuerzan la identidad de tu marca.</p>
-          </div>
-        <div className="serv-items">
-            <h3 className="serv-title2"><img src={stats} alt="stats" className="icon-servstats"/>Plan Estratégico Personalizado</h3>
-            <p className="serv-description2">Diseñamos estrategias tecnológicas a medida, alineadas con los objetivos de tu empresa para maximizar su potencial.</p>
-        </div>
-        <div className="serv-items">
-            <h3 className="serv-title3"><img src={nut} alt="Tuerquita" className="icon-servnut" />Implementaciones Tecnológicas</h3>
-            <p className="serv-description3">Desarrollamos soluciones digitales personalizadas para optimizar tus procesos y modernizar tu negocio.</p>
-        </div>
-        <div className="serv-items">
-            <h3 className="serv-title4"><img src={cerebro} alt="cerebro" className="icon-servcerebro" />Automatización con Inteligencia Artificial</h3>
-            <p className="serv-description4">Integramos IA para optimizar procesos, reducir costos y tomar decisiones más inteligentes en tiempo real.</p>
-        </div>
-    </section>
+    <div style={{ height: '600px', position: 'relative' }}>
+  <InfiniteMenu items={items}/>
+</div>
+
   );
 }
+
+const items = [
+  {
+    image: UIUX,
+    link: '../servicios.tsx',
+    title: 'Diseño UI/UX',
+    description: 'Creamos interfaces modernas, únicas y funcionales que mejoran la experiencia de usuario y refuerzan la identidad de tu marca.'
+  },
+  {
+    image: stats,
+    link: 'https://google.com/',
+    title: 'Plan Estratégico Personalizado',
+    description: 'Diseñamos estrategias tecnológicas a medida, alineadas con los objetivos de tu empresa para maximizar su potencial.'
+  },
+  {
+    image: nut,
+    link: 'https://google.com/',
+    title: 'Implementaciones Tecnológicas',
+    description: 'Desarrollamos soluciones digitales personalizadas para optimizar tus procesos y modernizar tu negocio.'
+  },
+  {
+    image: cerebro,
+    link: 'https://google.com/',
+    title: 'Automatización con Inteligencia Artificial',
+    description: 'Integramos IA para optimizar procesos, reducir costos y tomar decisiones más inteligentes en tiempo real.'
+  }
+];
+
+
+
+
+
