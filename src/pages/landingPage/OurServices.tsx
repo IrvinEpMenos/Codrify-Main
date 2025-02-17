dcda4e1f12cf20be22f569f431fc1de317a53958
JonathanMN <jonathan_4.2003@hotmail.com>
2025-02-17 11:35:50 -0600

    Ourservices Part Pre-Final
    Se modifico el diseño de la parte de nuestros servicios de la LandingPage y ademas se agrego una animacion

--- src/pages/landingPage/OurServices.tsx
+++ src/pages/landingPage/OurServices.tsx
@@ -1,46 +1,47 @@
-import InfiniteMenu from '../../../reactBits/InfiniteMenu/InfiniteMenu';
-import cerebro from "../../assets/icon/cerebro.png";
+import "./css/OurServices.css";
+import cerebro from "../../assets/img/cerebro.png";
 import UIUX from "../../assets/icon/UIUXICON.svg";
 import nut from "../../assets/icon/nut.svg";
-import stats from "../../assets/icon/stats.svg"
+import stats from "../../assets/icon/stats.svg";
+import PixelCard from '../../../reactBits/PixelCard/PixelCard';
 
 export default function Services() {
   return (
-    <div style={{ height: '600px', position: 'relative' }}>
-  <InfiniteMenu items={items}/>
-</div>
-
+    <>
+      <div className="serv">
+        <h2 className="title">Nuestros Servicios</h2>
+        <p className="subtitle">Todo lo que necesitas - en un solo sitio.</p>
+      </div>
+      <div className="serv-cards">
+      <PixelCard variant="blue">
+          <div className="card-UIUX">
+            <h3>Diseño UI/UX</h3>
+            <img src={UIUX} alt="UI/UX"/>
+            <p>Creamos interfaces modernas, únicas y funcionales que mejoran la experiencia de usuario y refuerzan la identidad de tu marca.</p>
+          </div>
+      </PixelCard>
+      <PixelCard variant="blue">
+          <div className="card-stats">
+            <h3>Plan Estratégico Personalizado</h3>
+            <img src={stats} alt="stats" className="icon-servstats" />
+            <p>Diseñamos estrategias tecnológicas a medida, alineadas con los objetivos de tu empresa para maximizar su potencial.</p>
+          </div>
+          </PixelCard>
+          <PixelCard variant="blue">
+          <div className="card-cerebro">
+            <h3>Automatización con Inteligencia Artificial</h3>
+            <img src={cerebro} alt="cerebro" className="icon-servcerebro" />
+            <p>Integramos IA para optimizar procesos, reducir costos y tomar decisiones más inteligentes en tiempo real.</p>
+          </div>
+          </PixelCard>
+          <PixelCard variant="blue">
+          <div className="card-nut">
+            <h3>Implementaciones Tecnológicas</h3>
+            <img src={nut} alt="Tuerquita" className="icon-servnut" />
+            <p>Desarrollamos soluciones digitales personalizadas para optimizar tus procesos y modernizar tu negocio.</p>
+          </div>
+          </PixelCard>
+      </div>
+    </>
   );
 }
-
-const items = [
-  {
-    image: UIUX,
-    link: '../servicios.tsx',
-    title: 'Diseño UI/UX',
-    description: 'Creamos interfaces modernas, únicas y funcionales que mejoran la experiencia de usuario y refuerzan la identidad de tu marca.'
-  },
-  {
-    image: stats,
-    link: 'https://google.com/',
-    title: 'Plan Estratégico Personalizado',
-    description: 'Diseñamos estrategias tecnológicas a medida, alineadas con los objetivos de tu empresa para maximizar su potencial.'
-  },
-  {
-    image: nut,
-    link: 'https://google.com/',
-    title: 'Implementaciones Tecnológicas',
-    description: 'Desarrollamos soluciones digitales personalizadas para optimizar tus procesos y modernizar tu negocio.'
-  },
-  {
-    image: cerebro,
-    link: 'https://google.com/',
-    title: 'Automatización con Inteligencia Artificial',
-    description: 'Integramos IA para optimizar procesos, reducir costos y tomar decisiones más inteligentes en tiempo real.'
-  }
-];
-
-
-
-
-
