243de0ec8b16a9c443d539b3decd0c969db27bd3
xThunderking <reynarayonacho850@gmail.com>
2025-02-19 14:20:06 -0600

    server3.5

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -28,7 +28,7 @@ const ContactPage: React.FC = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:10000", {
+      const response = await fetch("https://correo-server.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
