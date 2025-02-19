7a6e8b4f86996332f901e1a6ee487dcfb10fdb72
xThunderking <reynarayonacho850@gmail.com>
2025-02-19 13:57:04 -0600

    server3

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -30,7 +30,7 @@ const ContactPage: React.FC = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000", {
+      const response = await fetch("http://localhost:10000", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
