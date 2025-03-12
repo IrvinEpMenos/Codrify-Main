ba116c5ade30614b36a4bd5460efe8226f0baee6
Jonathan Morales <146413718+JonathanMN145@users.noreply.github.com>
2025-03-12 13:05:18 -0600

    RUBOTS Y SITEMAP
    Se agrego el archivo Robots.txt con el sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'  // Importar el plugin
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://codrify.site',  // Cambia esto por la URL de tu sitio
+    }),
+  ],
 })
